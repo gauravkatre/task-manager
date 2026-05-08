c70047c9db3f0906b47e1c148488bb48d90a3779
Gaurav Katre <gauravkatre@gmail.com>
2026-05-09 01:15:37 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  preview: {
+    allowedHosts: ['all'],
+    port: 8080,
+    host: true,
+  }
 })
